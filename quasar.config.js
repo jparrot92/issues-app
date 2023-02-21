55eb4a92c4127e24144c5ba8c8168c556b2b48b8
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-02-21 20:29:20 +0100

    VueQuery + ENVs + Quasar

--- quasar.config.js
+++ quasar.config.js
@@ -30,8 +30,7 @@ module.exports = configure(function (/* ctx */) {
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
     boot: [
-      
-      
+      'vue-query'
     ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
@@ -69,7 +68,7 @@ module.exports = configure(function (/* ctx */) {
 
       // publicPath: '/',
       // analyze: true,
-      // env: {},
+      env: require('dotenv').config().parsed,
       // rawDefine: {}
       // ignorePublicFolder: true,
       // minify: false,
